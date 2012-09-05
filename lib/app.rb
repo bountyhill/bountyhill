c7a705477c1847e4823f52777d3f9dfa2e475463
radiospiel <eno@open-lab.org>
2012-09-05 16:09:53 +0200

    Improves configuration: now supports a local config file.

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,20 @@
 module App
-  def self.configs
-    @configs ||= YAML.load File.read("config/app.yml")
+  def self.load_config_from_yaml_file(path)
+    configs = YAML.load File.read(path)
+
+    config, env_setting = *configs.values_at("default", Rails.env)
+    (config || {}).update(env_setting || {})
+  rescue Errno::ENOENT
+    W "Missing config file: #{path}"
+    {}
   end
   
   def self.config
-    @config ||= configs["default"]
+    @config ||= begin
+      config = load_config_from_yaml_file("config/app.defaults.yml")
+      local_config = load_config_from_yaml_file("config/app.local.yml")
+
+      config.merge local_config
+    end
   end
 end
