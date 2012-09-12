0bfe7fecb4fcf849c329ee73fa70b44e272a5ff8
radiospiel <eno@open-lab.org>
2012-09-12 13:25:24 +0200

    Some minor code changes:
    - refactored image_tag helper
    - only production uses Bountybase logger

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,25 +32,32 @@ module ApplicationHelper
   def image_for(quest, options = {})
     render_image_for "fullsize", quest, options
   end
+
+  def render_imgio_tag(original_image_url, options)
+    width, height = options.values_at(:width, :height)
+    options[:width], options[:height] = "100%", nil
+    url = "#{IMGIO}/jpg/50/fill/#{width}x#{height}/#{original_image_url}"
+    
+    image_tag url, options
+  end
   
   def render_image_for(size, quest, options)
     expect! size => String
+
+    options[:title] ||= quest.title
     
     width, height = options.values_at :width, :height
     if IMGIO && width && height && (url = quest.original_image_url)
-      url = "#{IMGIO}/jpg/50/fill/#{width}x#{height}/#{url}"
-    else
-      # The quest.image hash must have a size entry, which is a Hash
-      expect! quest.image => { size => Hash }
-      
-      image_data = quest.image[size]
-
-      options[:width], options[:height] = image_data.values_at("width", "height")
-      url = image_data["url"]
+      return render_imgio_tag(url, options)
     end
 
-    options[:title] ||= quest.title
-    image_tag url, options
+    # The quest.image hash must have a size entry, which is a Hash
+    expect! quest.image => { size => Hash }
+    
+    image_data = quest.image[size]
+    options[:width], options[:height] = image_data.values_at("width", "height")
+
+    image_tag image_data["url"], options
   end
   
   def show_company_footer?
