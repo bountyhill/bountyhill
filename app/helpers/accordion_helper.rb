da24ba81488fd2cf208e38acf836089aaa953835
radiospiel <eno@open-lab.org>
2012-09-16 20:43:38 +0200

    Accordion items cannot be extended if the description is blank.

--- app/helpers/accordion_helper.rb
+++ app/helpers/accordion_helper.rb
@@ -10,24 +10,35 @@ module AccordionHelper
 
     ARROW = '<span class="arrow"></span>'.html_safe
     
+    def item_header(item_id, title, mode)
+      expect! mode => [ :blank, :present ]
+      a = link_to h2(title), "##{item_id}", :class => "accordion-toggle", 
+        "data-toggle" => "collapse", "data-parent" => "##{@accordion_id}",
+        "data-bitly-type"=>"bitly_hover_card"
+      a += ARROW if mode == :present
+
+      div a, :class => "accordion-heading"
+    end
+
     def item(title, *contents, &block)
+      options = contents.extract_options!
+      expect! options => { :collapsed => [ true, false, nil ] }
+      
       contents << capture(&block) if block_given?
-
+      contents.compact!
+      
       generate_item_id do |item_id|
-        heading = div :class => "accordion-heading" do
-          a = link_to h2(title), "##{item_id}", :class => "accordion-toggle", 
-            "data-toggle" => "collapse", "data-parent" => "##{@accordion_id}",
-            "data-bitly-type"=>"bitly_hover_card"
-          a + ARROW
+        heading = item_header(item_id, title, contents.blank? ? :blank : :present)
+        if contents.present?
+          css = "accordion-body bg-gray collapse"
+          css += " in" if options[:collapsed] == false
+
+          body = div :id => item_id, :class => css do
+            div(*contents, :class => "accordion-inner")
+          end
         end
 
-        collapsed = !first_item?
-        
-        body = div :id => item_id, :class => "accordion-body bg-gray collapse #{collapsed ? "" : "in"}" do
-          div(*contents, :class => "accordion-inner")
-        end
-
-        div heading + body, :class=>"accordion-group"
+        div heading, body, :class=>"accordion-group"
       end.html_safe
     end
 
@@ -35,10 +46,6 @@ module AccordionHelper
     
     def generate_item_id
       yield "#{@accordion_id}_#{@item_id += 1}"
-    end
-    
-    def first_item?
-      @item_id == 1
     end
   end
 
