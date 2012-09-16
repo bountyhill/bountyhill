f86516a2c53a6e1ae9d93c5a6f640041ada2a12e
radiospiel <eno@open-lab.org>
2012-09-17 00:07:56 +0200

    Adds url support in action_buttons_helper

--- app/helpers/action_buttons_helper.rb
+++ app/helpers/action_buttons_helper.rb
@@ -9,13 +9,26 @@ module ActionButtonsHelper
       @html = []
     end
     
-    def action(icon, title, sub = nil)
+    # action "t", :title,  [ "sub", ] :url => url
+    # action "t", "title", [ "sub", ] :url => url
+    def action(icon, *args)
+      options = args.extract_options!
+      title, sub = *args
+
+      expect! title => [ Symbol, String ], sub => [ Symbol, String, nil ]
+
       if title.is_a?(Symbol)
         sub ||= I18n.t("#{title}.sub")
         title = I18n.t("#{title}.title")
       end
+      if sub.is_a?(Symbol)
+        sub = I18n.t(sub)
+      end
+      
+      url = options[:url] || '#'
+      
       li = self.li do
-        link_to "#", "data-bitly-type" => "bitly_hover_card" do
+        link_to url, "data-bitly-type" => "bitly_hover_card" do
           html = span icon, "class" => "ca-icon twitter"
           html += div "class" => "ca-content" do
             title = span title, :class => "ca-main"
@@ -38,7 +51,7 @@ module ActionButtonsHelper
   def action_buttons(options = {})
     ul options.merge(:class=>"ca-menu bg-gray") do
       ActionButtons.new(self).tap do |action_buttons|
-        Proc.new.bind(action_buttons).call
+        yield action_buttons
       end.html
     end
   end
