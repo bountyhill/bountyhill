ad01c466bd47ed1ef2e0c0c31614dfdfa3cad808
radiospiel <eno@open-lab.org>
2012-09-17 14:48:58 +0200

    Determine location even when running locally.
    The location is determined by asking a web "service."

--- lib/extensions/rack_extensions.rb
+++ lib/extensions/rack_extensions.rb
@@ -15,16 +15,42 @@ class Rack::Request
     OpenStruct.new city
   end
 
+
   # lookup an IP's location from the GeoLiteCity database.
   def geo_lite_city
     if @geo_lite_city.nil?
-      city = Rack::Request.geo_lite_city.city(ip)
-      @geo_lite_city = city ? city.to_hash : false
+      ip = self.ip
+      
+      # Try to resolve global IP if we are running locally.
+      if ip == "127.0.0.1"
+        ip = Rack::Request.find_global_ip 
+      end
+      
+      if ip
+        if city = Rack::Request.geo_lite_city.city(ip)
+          @geo_lite_city = city.to_hash
+        end
+      end
+      @geo_lite_city ||= false
     end
     
     @geo_lite_city || nil
   end
 
+  def self.find_global_ip
+    if @globalip.nil?
+      @globalip = begin
+        Timeout.timeout(5) {
+          Net::HTTP.get(URI('http://globalip.herokuapp.com'))
+        }
+      rescue Timeout::Error
+        false
+      end
+    end
+
+    @globalip || nil
+  end
+  
   def self.geo_lite_city #:nodoc:
     @geo_ip ||= ::GeoIP.new("data/GeoLiteCity.dat")
   end
