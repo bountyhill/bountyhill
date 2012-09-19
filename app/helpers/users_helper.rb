02b079ba4066e23ebd7acf5b1d3b4391c30e5c4c
radiospiel <eno@open-lab.org>
2012-09-20 01:07:07 +0200

    Show real avatar for quests.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,23 @@
 module UsersHelper
-  AVATAR_SIZE = 96
+  AVATAR_SIZE = 128
+  
+  def default_avatar
+    File.join(root_url, asset_path("default_avatar_128.png"))
+  end
   
   # Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user)
-    avatar_url = user.avatar :default => "#{root_url}/#{image_path("default_avatar.png")}", :size => AVATAR_SIZE
-    image_tag(avatar_url, alt: user.name, class: "avatar")
+  def avatar(user, options = {})
+    size = options[:size] ||= AVATAR_SIZE
+    default = options[:default] || default_avatar
+    
+    url = user.avatar(:size => size, :default => default)
+    image_tag url, alt: user.name, title: user.name, class: "avatar", 
+                 width: size, height: size
+  end
+
+  def link_to_twitter_account(user)
+    if twitter_handle = user.twitter_handle
+      link_to twitter_handle, "https://twitter.com/#{twitter_handle[1..-1]}"
+    end
   end
 end
