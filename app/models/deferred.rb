10f381640326f4add6f77b8911228e8a21457731
radiospiel <eno@open-lab.org>
2012-09-20 18:30:13 +0200

    The final Deferred module

--- app/models/deferred.rb
+++ app/models/deferred.rb
@@ -1,16 +1,26 @@
+# Deferred runs methods asynchonously.
 module Deferred
-  # -- implementations: all methods below can be run deferredly 
-  # via Deferred.name; e.g. Deferred.twitter runs Deferred#twitter
-  # in a background thread.
-  def self.method_missing(sym, *args)
+  def self.method_missing(sym, *args) #:nodoc:
     super unless instance_methods.include?(sym)
 
-    queue(name) << args
+    queue(sym) << args
   end
   
+  # return a queue with a specific name, which runs the Deferred#name action.
   def self.queue(name)
     @queues ||= {}
-    @queues[name] ||= GirlFriday::WorkQueue.new(name, :size => 1) do |*args|
+    @queues[name] ||= create_queue(name)
+  end
+  
+  def self.create_queue(name)
+    unless instance_methods.include?(name)
+      raise ArgumentError, "Invalid Deferred action #{name.inspect}" 
+    end
+    
+    GirlFriday::WorkQueue.new(name, :size => 1) do |args|
+      Thread.send :sleep, 0.3
+
+      instance = Object.new.extend(Deferred)
       instance.send name, *args
     end
   end
@@ -37,33 +47,9 @@ module Deferred
     client.send *args
   end
 
-  # -- run a twitter request ------------------------------------------
-  
-  TWITTER_CONFIG = Bountybase.config.twitter_app
-  
-  def twitter(*args)
-    oauth = args.extract_options!
-    
-    expect! oauth => {
-      :oauth_token  => String,
-      :oauth_secret => String
-    }
-
-    client = Twitter::Client.new(
-      :oauth_token        => oauth[:oauth_token],
-      :oauth_token_secret => oauth[:oauth_secret],
-      :consumer_key       => TWITTER_CONFIG["consumer_key"],
-      :consumer_secret    => TWITTER_CONFIG["consumer_secret"]
-    )
-
-    client.send *args
-  end
-
   # -- deliver an email -----------------------------------------------
   
-  def mail(email)
-    expect! email => Mail::Message
-    
+  def mail(mail)
     STDERR.puts "---- Sending email ------------------------\n#{mail}"
     mail.deliver
   end
