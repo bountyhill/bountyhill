207e9ee858f176602ecf47d162d61d128a316015
radiospiel <eno@open-lab.org>
2012-09-21 21:28:06 +0200

    Deferred can be set to run synchronously

--- app/models/deferred.rb
+++ app/models/deferred.rb
@@ -1,9 +1,19 @@
 # Deferred runs methods asynchonously.
 module Deferred
+  RUN_DEFERRED = true
+  
   def self.method_missing(sym, *args) #:nodoc:
     super unless instance_methods.include?(sym)
 
-    queue(sym) << args
+    if RUN_DEFERRED
+      queue(sym) << args
+    else
+      instance.send sym, *args
+    end
+  end
+  
+  def self.instance
+    Object.new.extend(self)
   end
   
   # return a queue with a specific name, which runs the Deferred#name action.
@@ -18,9 +28,7 @@ module Deferred
     end
     
     GirlFriday::WorkQueue.new(name, :size => 1) do |args|
-      Thread.send :sleep, 0.3
-
-      instance = Object.new.extend(Deferred)
+      Thread.send :sleep, 0.2
       instance.send name, *args
     end
   end
