15fd0a6bcadc4f775d8a7bf6e4ed888f67ffcbb0
radiospiel <eno@open-lab.org>
2012-09-22 21:51:04 +0200

    Fix identities indices.
    While the name must be unique amongst twitter identities, it must not be unique amongst all Identities (and usually is not.)

---
+++ db/migrate/014_fix_identity_indices.rb
@@ -0,0 +1,12 @@
+class FixIdentityIndices < ActiveRecord::Migration
+  def up
+    remove_index "identities", ["email"]
+    remove_index "identities", ["name", "type"]
+
+    add_index "identities", ["email", "type"], :unique => true
+    add_index "identities", ["name", "type"]
+  end
+
+  def down
+  end
+end
