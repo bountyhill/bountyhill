d7504f4d864515e7c0a8f9e1e8fc2cb7fb058f09
radiospiel <eno@open-lab.org>
2012-09-25 16:38:43 +0200

    Cleans up and renames halt module

--- app/controllers/application_controller/halt.rb
+++ app/controllers/application_controller/halt.rb
@@ -1,22 +1,33 @@
 #
-# ApplicationController::Redirected provides a redirect_to! method,
-# which works like redirect_to, but stops the action at that point.
-module ApplicationController::Redirected
+# ApplicationController::Halt adds a halt! method which halts the current
+# action, and render! and redirect_to! methods based on halt!, that work
+# like their non-bang counterparts but stop the action upon invocation.
+module ApplicationController::Halt
 
   def self.included(klass)
-    klass.rescue_from AfterRedirection, :with => :after_redirection
+    klass.rescue_from HaltCondition, :with => :halted
   end
 
-  # The AfterRedirection is raised by redirect!
-  class AfterRedirection < RuntimeError; end
+  # halt! raises the HaltCondition, which leaves the current action
+  # and ends up in the halted no-op method.
+  class HaltCondition < RuntimeError; end
 
-  def redirect_to!(*args)
-    redirect_to *args
-    raise AfterRedirection
-  end
-  
   private
   
-  def after_redirection
+  def redirect_to!(*args)
+    redirect_to *args
+    halt!
+  end
+
+  def render!(*args)
+    render *args
+    halt!
+  end
+  
+  def halt!
+    raise HaltCondition
+  end
+  
+  def halted
   end
 end
