b65383c2a976a90648e19e805156dd7447b7f505
radiospiel <eno@open-lab.org>
2012-10-09 12:47:39 +0200

    Fixes share path helpers.

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -2,14 +2,14 @@
 # a quest.
 class ShareController < ApplicationController
   # Show the form to share a quest
-  def show
+  def new
     request_identity! :twitter
-    @quest = Quest.find(params[:id])
+    @quest = Quest.find(params[:quest_id])
   end
   
   # start the run. This action receives the form from "runs/show".
   def create
-    @quest = Quest.find(params[:id])
+    @quest = Quest.find(params[:quest_id])
     current_user.retweet @quest
     
     redirect_to @quest, notice: "Thank your for sharing!"
