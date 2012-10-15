f759e50bd7f5ead10c7ed0269c8d89035d4b79d6
radiospiel <eno@open-lab.org>
2012-10-15 16:59:23 +0200

    Share the tweet-preview form between /runs and /shares; closes #55.
    This patch:
    
    - renames share_controller => shares_controller, because Rails routes should be pluralized anyways;
    - the id parameter now refers to a Quest, as in /runs
    - adjust routes and paths for shares
    - implements SharesController similar to RunsController.

---
+++ app/controllers/shares_controller.rb
@@ -0,0 +1,38 @@
+# The SharesController is used to retweet (and later potentially refacebook and whatever) 
+# a quest.
+class SharesController < ApplicationController
+  before_filter :quest
+  attr :quest
+  
+  # Show the form to share a quest
+  def show
+    request_identity! :twitter
+  end
+  
+  # start the run. This action receives the form from "runs/show".
+  def update
+    # Re-render the form, if the quest is not valid.
+    quest.attributes = params[:quest]
+    unless quest.valid?
+      flash.now[:error] = if base_errors = @quest.errors[:base]
+        I18n.t("quest.message.base_error", :base_error => base_errors.join(", "))
+      else
+        I18n.t("quest.message.error")
+      end
+
+      render! action: "show"
+    end
+    
+    # quest.tweet is a pseudo-attribute; it will be set from the form data.
+    current_user.retweet(quest, :message => quest.tweet)
+
+    flash[:success] = I18n.t("quest.shared", :title => quest.title)
+    redirect_to quests_path(:owner_id => current_user.id)
+  end
+
+  private
+  
+  def quest
+    @quest ||= Quest.find(params[:id])
+  end
+end
