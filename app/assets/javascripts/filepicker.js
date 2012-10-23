ffa6134a8f9509af24ee0bb75006b3adb3e0067a
radiospiel <eno@open-lab.org>
2012-10-23 20:12:06 +0200

    Adds images uploader to offers/new form, see #30

---
+++ app/assets/javascripts/filepicker.js
@@ -0,0 +1,35 @@
+/* 
+ * set filepicker defaults for pickMultiple and pick functions; 
+ * see https://developers.filepicker.io/docs/web
+ */
+jQuery(document).on("change", "input[type=filepicker]", function(event) {
+  var fpfiles = event.originalEvent.fpfile;
+  if(!fpfiles) return;
+
+  var width = 100;
+  var height = 140;
+  
+  var self = jQuery(this);
+  var form = self.closest("form");
+
+  /*
+   * The slideshow target node; this is a UL, and new images are
+   * added as <li><img src="..."></li> nodes.
+   */
+  var slideshow = (function() {
+    var slides = self.data("fp-slides");
+    return jQuery(slides, form);
+  })();
+  
+  jQuery.each(fpfiles, function() {
+    var url = this.url;
+    var slide_url = url + "/convert?w=" + width + "&h=" + height;
+  
+    var slide = $('<li><input type="hidden"></input><img src=""></img><span></span></li>');
+    slide.find("input").attr("name", self.data("fp-name")).val(url);
+    slide.find("img").attr("src", slide_url).attr("width", width).attr("height", height);
+    slide.find("span").text(this.filename);
+    
+    slideshow.append(slide);
+  });
+});
