11c39b02d7b1a1ce4d4efed20650d491ae50916f
Torsten Graf-Oettel <torsten.graf.oettel@gmail.com>
2012-10-31 09:18:13 +0100

    style 'action' stickers on quests/show

--- app/helpers/quests_helper.rb
+++ app/helpers/quests_helper.rb
@@ -1,31 +1,27 @@
 module QuestsHelper
   def render_bounty_badge(quest)
     expect! quest => [Quest, Offer]
-
-    amount = quest.bounty.to_f
-    return if amount <= 0
     
-    css = if amount < 100 then "small"
-      elsif amount < 1000 then "medium"
-      else "large"
-      end
-      
-    div quest.bounty.to_s(:cents => false), :class => "bounty_badge #{css}"
+    div quest.bounty.to_s(:cents => false), :class => "bounty_badge #{bounty_class(quest)}"
   end
   
   def render_bounty_ribbon(quest)
     expect! quest => [Quest, Offer]
-
+    
+    div :class => "ribbon-wrapper right" do
+      div quest.bounty.to_s(:cents => false), :class => "ribbon #{bounty_class(quest)}"
+    end
+  end
+  
+  def bounty_class(quest)
+    expect! quest => [Quest, Offer]
+    
     amount = quest.bounty.to_f
     return if amount <= 0
     
-    css = if amount < 100 then "small"
-      elsif amount < 1000 then "medium"
-      else "large"
-      end
-      
-    div :class => "ribbon-wrapper right" do
-      div quest.bounty.to_s(:cents => false), :class => "ribbon #{css}"
+    if    amount < 100  then  "small"
+    elsif amount < 1000 then  "medium"
+    else                      "large"
     end
   end
   
