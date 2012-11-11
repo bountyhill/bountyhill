7403d41a75a05fc419c88c49a16051f1d85e06c9
Torsten Graf-Oettel <torsten.graf.oettel@gmail.com>
2012-11-11 16:07:46 +0100

    remove HTML mocks

--- app/controllers/mocks_controller.rb
+++
@@ -1,9 +0,0 @@
-class MocksController < ApplicationController
-  
-  %w(quests offers).each do |mock|
-    define_method mock do
-      render "mocks/#{mock}/#{(params[:page] || "index")}"
-    end
-  end
-  
-end
