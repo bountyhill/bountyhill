504d2b51d72b81905b88501452f20f2e7b40a38a
radiospiel <eno@open-lab.org>
2012-11-12 14:27:19 +0100

    Conditionally enable deletion of comments; fixes #97
    A comment can be deleted by admins, by comment owners, and by owners of the commented object.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,13 @@ class Comment < ActiveRecord::Base
   opinio
 
   def self.default_per_page
-     10
-   end
+    10
+  end
+
+  def writable?(user = ActiveRecord::AccessControl.current_user)
+    return false unless user
+    return false unless commentable
+
+    user.admin? or (owner == user) or (commentable.owner == user)
+  end
 end
