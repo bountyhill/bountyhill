32597635beb80de66d06ec282c8e92a70b167ab1
Torsten Graf-Oettel <torsten.graf.oettel@gmail.com>
2013-02-25 22:57:31 +0100

    cleanup

---
+++ app/helpers/temp_helper.rb
@@ -0,0 +1,13 @@
+#TODO: this is just a temporary helper - remove when site is implemented
+
+module TempHelper
+  def placeholder(text, options={})
+    div :class => "placeholder", :style => options[:style] do
+      content_tag :b, "Placeholder: #{text}"
+    end
+  end
+  
+  def spacer(options={})
+    div "", :class => "spacer", :style => options[:style]
+  end
+end
