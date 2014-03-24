be526da82c8f5f83ecc7e7b69d05a80a7e9ad27a
Torsten Graf-Oettel <torsten.graf.oettel@gmail.com>
2014-03-24 08:48:59 +0100

    disable zoom in when enter form inputs on iOS Safari

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,6 +35,20 @@ jQuery.escapeHTML = function(str) {
 
 $(document).ready(function() {
 
+  // prevent Safari on iOS to zoom site
+  // see: http://stackoverflow.com/questions/2989263/disable-auto-zoom-in-input-text-tag-safari-on-iphone
+  $('textarea, input[type="text"], input[type="password"], input[type="search"], input[type="email"], input[type="url"], input[type="number"]').on({ 'touchstart' : function() { zoomDisable(); }});
+  $('textarea, input[type="text"], input[type="password"], input[type="search"], input[type="email"], input[type="url"], input[type="number"]').on({ 'touchend' : function() { setTimeout(zoomEnable, 500); }});
+
+  function zoomDisable(){
+    $('head meta[name="viewport"]').remove();
+    $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />');
+  }
+  function zoomEnable(){
+    $('head meta[name="viewport"]').remove();
+    $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />');
+  } 
+
   // enable endless scrolling
   // see http://blog.migrantstudios.com/2012/07/11/building-an-infinite-scroll-component-with-rails-and-jquery/
   $('.endless_scroll_hook').bind('inview', function(e,visible) {
