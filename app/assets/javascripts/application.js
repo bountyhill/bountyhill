859114a6974dc8f67159c42aeb9ba48e0112e9a4
Torsten Graf-Oettel <torsten.graf.oettel@gmail.com>
2014-04-13 13:53:32 +0200

    re-organize JS patches to re-use them in dialog layout as well

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 //= require bootstrap-lightbox
 //= require bootstrap-select
 //= require html5slider
+//= require patches
 
 // dummy console, just in case
 if(typeof console === "undefined") {
@@ -32,38 +33,3 @@ if(typeof console === "undefined") {
 jQuery.escapeHTML = function(str) {
   return jQuery('<div/>').text(str).html();
 };
-
-$(document).ready(function() {
-
-  // prevent Safari on iOS to zoom site
-  // see: http://stackoverflow.com/questions/2989263/disable-auto-zoom-in-input-text-tag-safari-on-iphone
-  $('textarea, input[type="text"], input[type="password"], input[type="search"], input[type="email"], input[type="url"], input[type="number"]').on({ 'touchstart' : function() { zoomDisable(); }});
-  $('textarea, input[type="text"], input[type="password"], input[type="search"], input[type="email"], input[type="url"], input[type="number"]').on({ 'touchend' : function() { setTimeout(zoomEnable, 500); }});
-
-  function zoomDisable(){
-    $('head meta[name="viewport"]').remove();
-    $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />');
-  }
-  function zoomEnable(){
-    $('head meta[name="viewport"]').remove();
-    $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />');
-  } 
-
-  // enable endless scrolling
-  // see http://blog.migrantstudios.com/2012/07/11/building-an-infinite-scroll-component-with-rails-and-jquery/
-  $('.endless_scroll_hook').bind('inview', function(e,visible) {
-    if(visible) {
-      $.getScript($(this).attr("href"));
-    }
-  });
-  
-  // use Ajax Content in Twitter Bootstrap Modal
-  // see http://blog.assimov.net/blog/2012/03/09/ajax-content-in-twitter-bootstrap-modal/
-  $("a[data-toggle=modal]").click(function (e) {
-   lv_target = $(this).attr('data-target');
-   lv_url = $(this).attr('href');
-   $(lv_target).load(lv_url);
-  });
-  
-});
-
