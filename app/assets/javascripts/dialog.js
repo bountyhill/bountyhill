48f3cc4f1a92f3e69608979a6b8718c00aa85f9d
Torsten Graf-Oettel <torsten.graf.oettel@gmail.com>
2014-09-23 09:07:55 +0200

    remove 'require jquery_ujs' from dialog.js to avoid multiple XHR submitting of forms - fixes #243

--- app/assets/javascripts/dialog.js
+++ app/assets/javascripts/dialog.js
@@ -1,4 +1,6 @@
+// Pls note:  Avoid 'require jquery_ujs' below since it leads to multiple include of this which causes
+//            remote forms to be sumbitted twice - see: http://stackoverflow.com/questions/4231885/rails-3-ujs-controller-gets-called-twice-by-link-to-remote
+
 //= require jquery
 //= require jquery-ui
-//= require jquery_ujs
 //= require patches
