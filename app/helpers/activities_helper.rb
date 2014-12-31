2d678412cafc793043d95c612c5509a610e722c5
Torsten Graf-Oettel <torsten.graf.oettel@gmail.com>
2014-12-31 14:14:47 +0100

    fix exception when displaying activities to non-logged-in users

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -4,6 +4,7 @@ module ActivitiesHelper
 
   def linked_title_of(activity)
     return unless activity.entity
+    return unless current_user
     return unless current_user.owns?(activity.entity) || (activity.entity.respond_to?(:user) && activity.entity.user == current_user)
     return unless (title_method = %w(title provider).detect{ |m| activity.entity.respond_to?(m) }).present?
     
