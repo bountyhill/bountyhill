0ad9d54dc5af47420e7054487d3c7589c2d672e2
Torsten Graf-Oettel <torsten.graf.oettel@gmail.com>
2015-01-10 15:15:56 +0100

    remove obsolete dialog.js - fixes #249

--- app/assets/javascripts/dialog.js
+++
@@ -1,6 +0,0 @@
-// Pls note:  Avoid 'require jquery_ujs' below since it leads to multiple include of this which causes
-//            remote forms to be sumbitted twice - see: http://stackoverflow.com/questions/4231885/rails-3-ujs-controller-gets-called-twice-by-link-to-remote
-
-//= require jquery
-//= require jquery-ui
-//= require patches
