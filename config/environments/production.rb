3869f8738796f9a21938e702f363e2232d6af667
Torsten Graf-Oettel <torsten.graf.oettel@gmail.com>
2015-01-10 15:06:28 +0100

    enable compression of assets - fixes #247

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,11 @@ Bountyhill::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
+
+  # Choose the compressors to use
+  config.assets.js_compressor  = :uglifier
+  config.assets.css_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
