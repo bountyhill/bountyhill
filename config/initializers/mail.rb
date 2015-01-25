7e08b850f1e8a55f451ec4448aaa6f7721410481
Torsten Graf-Oettel <torsten.graf.oettel@gmail.com>
2015-01-25 21:22:33 +0100

    try mailgun SMTP server - see #255

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,12 +3,18 @@
 if(Rails.env.production?)
 
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    # :address        => 'smtp.sendgrid.net',
+    # :port           => '587',
+    # :authentication => :plain,
+    # :user_name      => ENV['SENDGRID_USERNAME'],
+    # :password       => ENV['SENDGRID_PASSWORD'],
+    # :domain         => 'heroku.com',
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'bountyhill-staging.com',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
   }
   ActionMailer::Base.delivery_method = :smtp
   
